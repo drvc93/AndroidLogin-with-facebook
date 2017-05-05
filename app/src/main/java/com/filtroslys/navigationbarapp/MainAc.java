77fed6e40be13c3d9ec8c50ae0daa7de733d1c23
drvc93xD <drvc_1993@hotmail.com>
2017-05-05 15:07:38 -0500

    v2

--- app/src/main/java/com/filtroslys/navigationbarapp/MainAc.java
+++ app/src/main/java/com/filtroslys/navigationbarapp/MainAc.java
@@ -2,19 +2,29 @@ package com.filtroslys.navigationbarapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import  com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainAc extends AppCompatActivity {
 
     private TextView info;
     private LoginButton loginButton;
+    String email  = "";
+    String birt = "";
 
     private CallbackManager callbackManager;
 
@@ -32,13 +42,26 @@ public class MainAc extends AppCompatActivity {
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                info.setText(
-                        "User ID: "
-                                + loginResult.getAccessToken().getUserId()
-                                + "\n" +
-                                "Auth Token: "
-                                + loginResult.getAccessToken().getToken()
-                );
+
+                GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        try {
+                             email  = (String) object.getString("email");
+                             birt = (String) object.get("birthday");
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email,gender,birthday");
+                request.setParameters(parameters);
+                request.executeAsync();
+                Log.i("DATOS", email);
+                Log.i("DATOS", birt);
+
             }
 
             @Override
@@ -52,6 +75,9 @@ public class MainAc extends AppCompatActivity {
             }
         });
     }
+
+
+
 
 
 }
